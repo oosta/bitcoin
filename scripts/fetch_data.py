a3b96bd455c8b51f4fac3881cd44fdab8200a070
Mahyar <mahyar.arjmand@gmail.com>
2024-07-06 17:56:58 -0400

    script for fetching and storing data

---
+++ scripts/fetch_data.py
@@ -0,0 +1,18 @@
+import requests
+import pandas as pd
+
+def fetch_bitcoin_data():
+    url = "https://api.coingecko.com/api/v3/coins/bitcoin/market_chart"
+    params = {
+        "vs_currency": "usd",
+        "days": "30"  # Fetch data for the last 30 days
+    }
+    response = requests.get(url, params=params)
+    data = response.json()
+    prices = data['prices']
+    df = pd.DataFrame(prices, columns=['timestamp', 'price'])
+    df['timestamp'] = pd.to_datetime(df['timestamp'], unit='ms')
+    return df
+
+df = fetch_bitcoin_data()
+print(df.head())
